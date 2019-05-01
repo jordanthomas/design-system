18053112e28334c8681df3acf8278bea8a37fb55
Ryan Hamilton <ryan@ryanahamilton.com>
2019-05-01 15:31:29 -0400

    additional component migration

---
+++ src/Button/Button.js
@@ -0,0 +1,82 @@
+// @flow
+/* eslint-disable react/button-has-type */
+
+import React, { type StatelessFunctionalComponent, type Node } from 'react';
+import classNames from 'classnames';
+import { LoadingDots } from '../LoadingDots';
+
+import './Button.scss';
+
+type ButtonDisplay =
+  | 'block'
+  | 'icon-only'
+  | 'icon-only-circle'
+  ;
+
+type ButtonSize =
+  | 'extra-sm'
+  | 'sm'
+  | 'lg'
+  | 'xl'
+  ;
+
+type ButtonStyle =
+  | 'primary'
+  | 'primary-important'
+  | 'primary-contrast'
+  | 'primary-danger'
+  | 'primary-light'
+  | 'secondary'
+  | 'secondary-important'
+  | 'secondary-danger'
+  | 'link'
+  ;
+
+export const Button: StatelessFunctionalComponent<{
+  buttonDisplay?: ButtonDisplay,
+  buttonSize?: ButtonSize,
+  buttonStyle?: ButtonStyle,
+  children?: Node,
+  className?: string,
+  disabled?: boolean,
+  inFlight?: boolean,
+  type?: 'button' | 'submit' | 'reset',
+}> = ({
+  buttonDisplay,
+  buttonSize,
+  buttonStyle,
+  children,
+  className,
+  disabled,
+  inFlight = false,
+  type = 'button',
+  ...otherProps
+}) => {
+  const isDisabled = disabled || inFlight;
+
+  const buttonClasses = classNames(
+    'btn',
+    {
+      [`btn--${buttonDisplay || ''}`]: !!buttonDisplay,
+      [`btn--${buttonSize || ''}`]: !!buttonSize,
+      [`btn--${buttonStyle || ''}`]: !!buttonStyle,
+    },
+    className,
+  );
+
+  return (
+    <button
+      type={type}
+      className={buttonClasses}
+      disabled={isDisabled}
+      {...otherProps}
+    >
+      {inFlight &&
+        <LoadingDots />
+      }
+      {!inFlight &&
+        children
+      }
+    </button>
+  );
+};
---
+++ src/LoadingDots/LoadingDots.js
@@ -0,0 +1,26 @@
+// @flow
+
+import React, { type StatelessFunctionalComponent } from 'react';
+import classNames from 'classnames';
+
+import './LoadingDots.scss';
+
+export const LoadingDots: StatelessFunctionalComponent<{
+  className?: string,
+}> = ({
+  className,
+  ...cleanedProps
+}) => {
+  const dotsClasses = classNames(
+    'loading-dots',
+    className,
+  );
+
+  return (
+    <span className={dotsClasses} {...cleanedProps}>
+      <span className="loading-dots__dot" />
+      <span className="loading-dots__dot" />
+      <span className="loading-dots__dot" />
+    </span>
+  );
+};
