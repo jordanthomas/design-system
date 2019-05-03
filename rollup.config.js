02b319826b3549776170ce74c84ff62b713a3492
Brandon Patram <brandon.patram@ebth.com>
2019-05-03 10:43:11 -0400

    move babel config out to babel config file

--- rollup.config.js
+++ rollup.config.js
@@ -42,22 +42,8 @@ export default [
       flowEntry({ mode: 'strict-local' }),
       // use babel to transform our source files
       babel({
-        // we dont want to bundle in all our dependencies, the end user's build
-        // system will handle this. this will also keep our dist lib size down
-        exclude: 'node_modules/**',
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              // by default babel will try to convert ES6 classes to CJS classes
-              // we dont want to do this since end users can consume classes just fine
-              modules: false
-            }
-          ],
-          ['@babel/preset-react', {}],
-          // strip out flow typings
-          ['@babel/preset-flow', {}]
-        ]
+        // pick up the shared babel.config.js/.bablerc file for config
+        babelrc: true
       })
     ],
     // TODO: just pull in from package.json peerDependencies and dependencies
