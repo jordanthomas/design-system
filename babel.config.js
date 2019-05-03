26e67ad5a2e4d9c53f5110b993dc15abb06f2474
Brandon Patram <brandon.patram@ebth.com>
2019-05-03 11:13:41 -0400

    setup testing config WIP

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,8 @@ module.exports = {
   // system will handle this. this will also keep our dist lib size down
   exclude: 'node_modules/**',
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        // by default babel will try to convert ES6 classes to CJS classes
-        // we dont want to do this since end users can consume classes just fine
-        modules: false
-      }
-    ],
+    ['@babel/preset-env', {}],
+    // convert jsx to js
     ['@babel/preset-react', {}],
     // strip out flow typings
     ['@babel/preset-flow', {}]
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  clearMocks: true,
+  setupFiles: ['<rootDir>/testSetup.js'],
+  rootDir: 'src',
+  collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**']
+};
