bec922ca97889c8cc636a98b882b382ebbac6eb3
Brandon Patram <brandon.patram@ebth.com>
2019-05-06 15:52:47 -0400

    jest to report junit for circle

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   clearMocks: true,
   setupFiles: ['<rootDir>/testSetup.js'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteName: 'Jest Test',
+        outputDirectory: 'reports',
+        outputName: 'jest.xml'
+      }
+    ]
+  ],
   rootDir: 'src',
   collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**'],
   moduleNameMapper: {
