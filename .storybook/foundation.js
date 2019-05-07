0b97632df532b6a39c0371907523f9b26291f153
Ryan Hamilton <ryan@ryanahamilton.com>
2019-05-07 09:41:54 -0400

    DRY up some markup

--- .storybook/foundation.js
+++ .storybook/foundation.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { storiesOf } from '@storybook/react';
+
+function renderColor(variable, hex, contrast: false, brand: false) {
+  const colorClasses = classNames(
+    'palette-grouping__color',
+    {
+      'palette-grouping__color--contrast': contrast,
+      'palette-grouping__color--brand': brand,
+    },
+  );
+
+  return (
+    <div className={colorClasses} style={{ backgroundColor: hex }}>
+      <div>{variable}</div>
+      <div>{hex}</div>
+    </div>
+  );
+}
 
 storiesOf('Foundation', module)
   .add('Home', () => (
@@ -15,101 +33,41 @@ storiesOf('Foundation', module)
       <div className="pallete-grid u-my4">
 
         <div className="palette-grouping">
-          <div className="palette-grouping__color palette-grouping__color--contrast palette-grouping__color--brand" style={{ backgroundColor: '#030102' }}>
-            <div>$black</div>
-            <div>#030102</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#292728' }}>
-            <div>$gray-500</div>
-            <div>#292728</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#747374' }}>
-            <div>$gray-400</div>
-            <div>#747374</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#a9a9a9' }}>
-            <div>$gray-300</div>
-            <div>#a9a9a9</div>
-          </div>
-          <div className="palette-grouping__color" style={{ backgroundColor: '#cfcfcf' }}>
-            <div>$gray-200</div>
-            <div>#cfcfcf</div>
-          </div>
-          <div className="palette-grouping__color" style={{ backgroundColor: '#f5f5f5' }}>
-            <div>$gray-100</div>
-            <div>#f5f5f5</div>
-          </div>
+          {renderColor('$black',    '#030102', true, true)}
+          {renderColor('$gray-500', '#292728', true)}
+          {renderColor('$gray-400', '#747374', true)}
+          {renderColor('$gray-300', '#a9a9a9', true)}
+          {renderColor('$gray-200', '#cfcfcf')}
+          {renderColor('$gray-100', '#f5f5f5')}
         </div>
 
         <div className="palette-grouping">
-          <div className="palette-grouping__color palette-grouping__color--contrast palette-grouping__color--brand" style={{ backgroundColor: '#d6a461' }}>
-            <div>$gold-300</div>
-            <div>#d6a461</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--brand" style={{ backgroundColor: '#faf6ed' }}>
-            <div>$gold-100</div>
-            <div>#faf6ed</div>
-          </div>
+          {renderColor('$gold-300', '#d6a461', true, true)}
+          {renderColor('$gold-100', '#faf6ed', false, true)}
         </div>
 
         <div className="palette-grouping">
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#003a49' }}>
-            <div>$teal-500</div>
-            <div>#003a49</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#387483' }}>
-            <div>$teal-300</div>
-            <div>#387483</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--brand" style={{ backgroundColor: '#a8d7dd' }}>
-            <div>$teal-100</div>
-            <div>#a8d7dd</div>
-          </div>
+          {renderColor('$teal-500', '#003a49', true)}
+          {renderColor('$teal-300', '#387483', true)}
+          {renderColor('$teal-100', '#a8d7dd', false, true)}
         </div>
 
         <div className="palette-grouping">
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#364919' }}>
-            <div>$green-500</div>
-            <div>#364919</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--contrast palette-grouping__color--brand" style={{ backgroundColor: '#718d50' }}>
-            <div>$green-300</div>
-            <div>#718d50</div>
-          </div>
-          <div className="palette-grouping__color" style={{ backgroundColor: '#aebf94' }}>
-            <div>$green-100</div>
-            <div>#aebf94</div>
-          </div>
+          {renderColor('$green-500', '#364919', true)}
+          {renderColor('$green-300', '#718d50', true, true)}
+          {renderColor('$green-100', '#aebf94')}
         </div>
 
         <div className="palette-grouping">
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#856100' }}>
-            <div>$yellow-500</div>
-            <div>#856100</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--brand" style={{ backgroundColor: '#edcb3a' }}>
-            <div>$yellow-300</div>
-            <div>#edcb3a</div>
-          </div>
-          <div className="palette-grouping__color" style={{ backgroundColor: '#ffe473' }}>
-            <div>$yellow-100</div>
-            <div>#ffe473</div>
-          </div>
+          {renderColor('$yellow-500', '#856100', true)}
+          {renderColor('$yellow-300', '#edcb3a', false, true)}
+          {renderColor('$yellow-100', '#ffe473')}
         </div>
 
         <div className="palette-grouping">
-          <div className="palette-grouping__color palette-grouping__color--contrast" style={{ backgroundColor: '#91271f' }}>
-            <div>$red-500</div>
-            <div>#91271f</div>
-          </div>
-          <div className="palette-grouping__color palette-grouping__color--brand" style={{ backgroundColor: '#e05d48' }}>
-            <div>$red-300</div>
-            <div>#e05d48</div>
-          </div>
-          <div className="palette-grouping__color" style={{ backgroundColor: '#ea9a92' }}>
-            <div>$red-100</div>
-            <div>#ea9a92</div>
-          </div>
+          {renderColor('$red-500', '#91271f', true)}
+          {renderColor('$red-300', '#e05d48', false, true)}
+          {renderColor('$red-100', '#ea9a92')}
         </div>
 
       </div>
