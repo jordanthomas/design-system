6ab9f87ba712faf29cc4fbe8f1e2def468d4b2ee
Ryan Hamilton <ryan@ryanahamilton.com>
2019-08-22 14:59:41 -0400

    moar prettier

--- .storybook/foundation.stories.js
+++ .storybook/foundation.stories.js
@@ -3,17 +3,12 @@ import { storiesOf } from '@storybook/react';
 import ColorBlock from './components/ColorBlock';
 
 storiesOf('Base|Foundation', module)
-  .add('Home', () => (
-    <div className="u-p8">
-      <h1>EBTH Design System</h1>
-    </div>
-  ))
+  .add('Home', () => <h1>EBTH Design System</h1>)
   .add('Color', () => (
-    <div className="u-p8">
+    <>
       <h1>Color Palette</h1>
 
       <div className="pallete-grid u-my4">
-
         <div className="palette-grouping">
           <ColorBlock variable="$black" hex="#030102" needsContrast isBrand />
           <ColorBlock variable="$gray-500" hex="#292728" needsContrast />
@@ -24,7 +19,12 @@ storiesOf('Base|Foundation', module)
         </div>
 
         <div className="palette-grouping">
-          <ColorBlock variable="$gold-300" hex="#d6a461" needsContrast isBrand />
+          <ColorBlock
+            variable="$gold-300"
+            hex="#d6a461"
+            needsContrast
+            isBrand
+          />
           <ColorBlock variable="$gold-100" hex="#faf6ed" isBrand />
         </div>
 
@@ -36,7 +36,12 @@ storiesOf('Base|Foundation', module)
 
         <div className="palette-grouping">
           <ColorBlock variable="$green-500" hex="#364919" needsContrast />
-          <ColorBlock variable="$green-300" hex="#718d50" needsContrast isBrand />
+          <ColorBlock
+            variable="$green-300"
+            hex="#718d50"
+            needsContrast
+            isBrand
+          />
           <ColorBlock variable="$green-100" hex="#aebf94" />
         </div>
 
@@ -51,15 +56,14 @@ storiesOf('Base|Foundation', module)
           <ColorBlock variable="$red-300" hex="#e05d48" isBrand />
           <ColorBlock variable="$red-100" hex="#ea9a92" />
         </div>
-
       </div>
 
       <h2>Accessibility</h2>
       <p>TK</p>
-    </div>
+    </>
   ))
   .add('Typography', () => (
-    <div className="u-p8">
+    <>
       <h1 className="u-mb4">Typography</h1>
 
       <div className="type-grid">
@@ -77,17 +81,23 @@ storiesOf('Base|Foundation', module)
           <div className="u-mb4">
             <p className="body--large-bold">Body Large: Aktiv 600 18/25</p>
             <p className="body--large">Body Large: Aktiv 400 18/25</p>
-            <p className="body--large-italic">Body Large: Aktiv 400 Italic 18/25</p>
+            <p className="body--large-italic">
+              Body Large: Aktiv 400 Italic 18/25
+            </p>
           </div>
           <div className="u-mb4">
             <p className="body--regular-bold">Body Regular: Aktiv 600 16/22</p>
             <p className="body--regular">Body Regular: Aktiv 400 16/22</p>
-            <p className="body--regular-italic">Body Regular: Aktiv 400 Italic 16/22</p>
+            <p className="body--regular-italic">
+              Body Regular: Aktiv 400 Italic 16/22
+            </p>
           </div>
           <div className="u-mb4">
             <p className="body--small-bold">Body Small: Aktiv 600 14/20</p>
             <p className="body--small">Body Small: Aktiv 400 14/20</p>
-            <p className="body--small-italic">Body Small: Aktiv 400 Italic 14/20</p>
+            <p className="body--small-italic">
+              Body Small: Aktiv 400 Italic 14/20
+            </p>
           </div>
         </div>
         <div>
@@ -104,20 +114,25 @@ storiesOf('Base|Foundation', module)
           <div className="u-mb4">
             <p className="body--large-bold">Body Large: Aktiv 600 18/25</p>
             <p className="body--large">Body Large: Aktiv 400 18/25</p>
-            <p className="body--large-italic">Body Large: Aktiv 400 Italic 18/25</p>
+            <p className="body--large-italic">
+              Body Large: Aktiv 400 Italic 18/25
+            </p>
           </div>
           <div className="u-mb4">
             <p className="body--regular-bold">Body Regular: Aktiv 600 16/22</p>
             <p className="body--regular">Body Regular: Aktiv 400 16/22</p>
-            <p className="body--regular-italic">Body Regular: Aktiv 400 Italic 16/22</p>
+            <p className="body--regular-italic">
+              Body Regular: Aktiv 400 Italic 16/22
+            </p>
           </div>
           <div className="u-mb4">
             <p className="body--small-bold">Body Small: Aktiv 600 14/20</p>
             <p className="body--small">Body Small: Aktiv 400 14/20</p>
-            <p className="body--small-italic">Body Small: Aktiv 400 Italic 14/20</p>
+            <p className="body--small-italic">
+              Body Small: Aktiv 400 Italic 14/20
+            </p>
           </div>
         </div>
       </div>
-
-    </div>
+    </>
   ));
--- .storybook/utility.stories.js
+++ .storybook/utility.stories.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Base|Utility', module)
-  .add('Spacing', () => (
-    <div className="u-p8">
-      <h1>Spacing Utilities</h1>
-    </div>
-  ));
+storiesOf('Base|Utility', module).add('Spacing', () => (
+  <h1>Spacing Utilities</h1>
+));
